57c4d975452535133847f415f713f22c8e6559a6
Bruno <bruno.kirchmeier@gmail.com>
2021-06-03 21:44:22 +0200

    Update: MVC erstellen

--- public/js/Controllers/todo-controller.js
+++ public/js/Controllers/todo-controller.js
@@ -7,114 +7,91 @@ class TodoController {
     constructor() {
         /** Model instanzieren */
         this.oNotizService = new NotizService();
-        /** DOM Elemente laden zu Index Page */
-        this.oNodeHeader = document.querySelector('.header');
-        this.oNodeContent = document.querySelector('.content');
-        /** Templates laden zu Index Page */
-        this.headerTemplateCompiled = Handlebars.compile(document.querySelector('.page-index.notiz-liste-header-template').innerHTML);
-        this.contentTemplateCompiled = Handlebars.compile(document.querySelector('.page-index.notiz-liste-content-template').innerHTML);
     }
 
     /** Public Methoden */
+    renderPage(event) {
+        /* Allgemeine DOM Selektoren */
+        let oNodeHeader = document.querySelector('.header');
+        let oNodeContent = document.querySelector('.content');
+        let oNodeFooter = document.querySelector('.footer');
+        let headerTemplateCompiled = null;
+        let contentTemplateCompiled = null;
+        /* PAGE: Index */
+        if (event === undefined
+            || event.target.dataset.changeToPage === 'index') {
+            /* Aktuelle Ansicht löschen */
+            oNodeHeader.innerHtml = '';
+            oNodeContent.innerHtml = '';
+            /* View Template laden und in DOM einsetzen */
+            headerTemplateCompiled = Handlebars.compile(document.querySelector('.page-index.notiz-liste-header-template').innerHTML);
+            contentTemplateCompiled = Handlebars.compile(document.querySelector('.page-index.notiz-liste-content-template').innerHTML);
+            oNodeHeader.innerHTML = headerTemplateCompiled();
+            oNodeContent.innerHTML = contentTemplateCompiled(
+                {notiz: this.oNotizService.getDatensaetze()},
+                {allowProtoPropertiesByDefault: true}
+            );
+            // window.addEventListener('DOMContentLoaded', (event) => {
+                oNodeHeader = document.querySelector('.header');
+                oNodeContent = document.querySelector('.content');
+                oNodeFooter = document.querySelector('.footer');
+
+                /* Klasse für Page Bezeichung ändern */
+                oNodeHeader.classList.remove('page-detail');
+                oNodeHeader.classList.add('page-index');
+                oNodeContent.classList.remove('page-detail');
+                oNodeContent.classList.add('page-index');
+                oNodeFooter.classList.remove('page-detail');
+                oNodeFooter.classList.add('page-index');
+                /* Eventlistener Registrieren */
+                let oNodeButtonNeueNotiz = document.querySelector('.button-notiz-neu');
+                let oNodeButtonEditNotiz = document.querySelector('.button-notiz-edit');
+                oNodeButtonNeueNotiz.addEventListener('click', this.renderPage);
+                oNodeButtonEditNotiz.addEventListener('click', this.renderPage);
+                // oNodeButtonNeueNotiz.addEventListener('click', () => { this.renderPage(); });
+                // oNodeButtonEditNotiz.addEventListener('click', () => { this.renderPage(); });
+                // oNodeButtonNeueNotiz.addEventListener('click', this.renderPage.bind(this));
+                // oNodeButtonEditNotiz.addEventListener('click', this.renderPage.bind(this));
+            // });
+        /* PAGE: Detail */
+        } else if (event.target.dataset.changeToPage === 'detail') {
+            /* Aktuelle Ansicht löschen */
+            oNodeHeader.innerHtml = '';
+            oNodeContent.innerHtml = '';
+            /* View Template laden und in DOM einsetzen */
+            contentTemplateCompiled = Handlebars.compile(document.querySelector('.page-detail.notiz-detail-template').innerHTML);
+            oNodeContent.innerHTML = contentTemplateCompiled();
+            /* Klasse für Page Bezeichung ändern */
+            // window.addEventListener('DOMContentLoaded', (event) => {
+                oNodeHeader = document.querySelector('.header');
+                oNodeContent = document.querySelector('.content');
+                oNodeFooter = document.querySelector('.footer');
+
+                oNodeHeader.classList.remove('page-index');
+                oNodeHeader.classList.add('page-detail');
+                oNodeContent.classList.remove('page-index');
+                oNodeContent.classList.add('page-detail');
+                oNodeFooter.classList.remove('page-index');
+                oNodeFooter.classList.add('page-detail');
+                /* Eventlistener Registrieren */
+                let oNodeButtonCancel = document.querySelector('.cancel');
+                oNodeButtonCancel.addEventListener('click', this.renderPage);
+                // oNodeButtonNeueNotiz.addEventListener('click', () => { this.renderPage(); });
+                // oNodeButtonCancel.addEventListener('click', this.renderPage.bind(this));
+            //  });
+        }
+    }
 
     /** Datensatz speichern */
-    saveDatensatz(event) {
+    saveDatensatz() {
     }
 
-    /** VIEW RENDERING: Ausgabe Page Index */
-    zeigePage(event) {
-        if (event != null) {
-            // console.log(event.target.dataset.changeToPage);
-        }
-
-        /** Header aufbereiten und Rendern */
-        this.oNodeHeader.innerHTML = this.headerTemplateCompiled();
-
-        /** Content aufbereiten und Rendern */
-        this.oNodeContent.innerHTML = this.contentTemplateCompiled(
-                {notiz: this.oNotizService.getDatensaetze()},
-                {allowProtoPropertiesByDefault: true});
-
-        if (event != null) {
-            this.initEventHandlers();
-        } else {
-            this.initEventHandlers(null);
-        }
-    }
-
-
-
-    /** VIEW RENDERING: Ausgabe Page Index */
-    zeigePageIndex(event) {
-        if (event != null) {
-            // console.log(event.target.dataset.changeToPage);
-        }
-
-        /** Header aufbereiten und Rendern */
-        this.oNodeHeader.innerHTML = this.headerTemplateCompiled();
-
-        /** Content aufbereiten und Rendern */
-        this.oNodeContent.innerHTML = this.contentTemplateCompiled(
-                {notiz: this.oNotizService.getDatensaetze()},
-                {allowProtoPropertiesByDefault: true});
-
-        if (event != null) {
-            this.initEventHandlers();
-        } else {
-            this.initEventHandlers(null);
-        }
-    }
-
-    /** VIEW RENDERING: Ausgabe Page detail */
-    zeigePageDetail(event) {
-        /** Header aufbereiten und Rendern */
-        this.oNodeHeader = document.querySelector('.header');
-        this.oNodeHeader.innerHTML = '';
-
-        /** Content aufbereiten und Rendern */
-        this.oNodeContent = document.querySelector('.content');
-        this.contentTemplateCompiled = Handlebars.compile(document.querySelector('.page-detail.notiz-detail-template').innerHTML);
-        this.oNodeContent.innerHTML = this.contentTemplateCompiled(
-                {notiz: this.oNotizService.getDatensaetze()},
-                {allowProtoPropertiesByDefault: true});
-
-        if(event != null) {
-            this.initEventHandlers();
-        } else {
-            this.initEventHandlers(null);
-        }
-    }
-
-    /** VIEW RENDERING: Eventlistener Registrieren */
-    initEventHandlers(event) {
-        /** Instanzierung Objekt */
-        if (event == null) {
-            /** DOM Elemente laden */
-            const oNodeButtonEditNotiz = document.querySelector('.button-notiz-edit');
-            const oNodeButtonNeueNotiz = document.querySelector('.button-notiz-neu');
-            const oNodeButtonSortieren = document.querySelector('.header-row-2-col-1');
-            const oNodeButtonFilter = document.querySelector('.button-zeige-abgeschlossen');
-            const oNodeButtonStyle = document.querySelector('.header-row-1-col-3');
-            /** Eventlistener Registrieren */
-            oNodeButtonNeueNotiz.addEventListener('click', this.zeigePageDetail);
-            oNodeButtonEditNotiz.addEventListener('click', this.zeigePageDetail);
-            /*
-            oNodeButtonSortieren.addEventListener('click', this.changeSort);
-            oNodeButtonFilter.addEventListener('click', this.changeFilter);
-            oNodeButtonStyle.addEventListener('click', this.changeStyle);
-            */
-
-        /** Event Aufruf */
-        } else {
-        }
-    }
-
-    init() {
-        /** Initialseite laden */
-        this.zeigePageIndex(null);
+    /** Datensatz speichern */
+    getDatensaetze() {
+        return this.oNotizService.getDatensaetze();
     }
 }
 
 /* Instanzierung Controller */
 const oTodo = new TodoController();
-oTodo.init();
+oTodo.renderPage();
